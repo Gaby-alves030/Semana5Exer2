1df2e6a08c41ffd2e651b3216d3b4de4b177a086
Gaby <gaabyrocha574@gmail.com>
2021-09-02 18:56:15 +0000

    Exer2

---
+++ Exer2.java
@@ -0,0 +1,23 @@
+public class Exer2{
+
+    public static void main(String args[]){
+        int x = 13;
+        while(x != 1){
+            if(x % 2 == 0){
+               x= x/2;
+           }else {
+              x= 3* x + 1;
+}
+     System.out.print(x + "->");
+     //x=40
+     //x=20
+     //x=10
+     //x=5
+     //x=16
+     //x=8
+     //x=4
+     //x=2
+     //x=1
+    }
+}
+}
